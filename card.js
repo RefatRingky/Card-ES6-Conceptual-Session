9510564837a12d55caa91a3253c3336ef5b8a7a0
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-27 00:58:20 +0600

    last commit

--- card.js
+++ card.js
@@ -1,15 +1,37 @@
-fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=2')
+const main =document.getElementById('main')
 // button onclick handler
 const searchButton = () =>{
     // input value
     const input = document.getElementById('input-value');
     const error = document.getElementById('error')
     const inputValue = input.value;
-    if(isNaN(inputValue)){
+   
+    if(isNaN(inputValue) || inputValue==""){
         // alert('Please enter valid number');
-        error.innerText = 'Please Enter an Number'
+        error.innerText = 'Please Enter an Number';
+        input.value= "";
     }
-    input.value= "";
+    else if(inputValue <0){
+        error.innerText = "Plaese enter positive value";
+        input.value= "";
+    }
+    else{
+        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
+        .then(res => res.json())
+        .then(data => cardDisplay(data.cards))
+        input.value= "";
+    }
+    
+}
+
+const cardDisplay = (cards) =>{
+    console.log(cards);
+    for(const card of cards){
+        const div = document.createElement('div');
+        div.innerHTML = `
+        
+        `
+    }
 }
 
 
